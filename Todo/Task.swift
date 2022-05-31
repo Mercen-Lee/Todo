69849bbe83f53a3d75048d71e6b21ea5746dae10
이석호 <mercen@mercen.net>
2022-05-31 22:02:47 +0900

    A Minor Update

---
+++ Todo/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  Todo
+//
+//  Created by Mercen on 2022/05/31.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool
+}
--- Todo/ViewController.swift
+++ Todo/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var choiTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "할 일 등록", message: "할 일을 입력해주세요", preferredStyle: .actionSheet)
+        let addButton = UIAlertAction(title: "등록", style: .default) { [weak self] _ in
+            <#code#>
+        }
+    }
+    
 }
 
